68cbf47780af67ea2bf3fcaa4b598cbafbc45b93
tonybutzer <tonybutzer@gmail.com>
2019-02-21 21:20:15 +0000

    automatic git update from Makefile

---
+++ lib/litestacLib/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(name='litestacLib',
+      maintainer='Tony Butzer',
+      maintainer_email='tonybutzer@gmail.com',
+      version='1.0.1',
+      description='helper functions for processing an espa order',
+      packages=[
+          'litestacLib',
+      ],
+      install_requires=[
+          'sat-search',
+      ],
+
+      )
